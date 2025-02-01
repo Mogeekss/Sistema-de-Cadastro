07f6ad032cacfdb8d7adce488298567d73617c17
Mogeekss <monicageeks1@gmail.com>
2025-02-01 14:27:03 -0300

    Sistema Cadastro

---
+++ Sistema_cadastro/src/sistema_cadastro/Sistema_cadastro.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistema_cadastro;
+
+import com.cadastro.telas.Tela_login;
+
+/**
+ *
+ * @author Lab.SG.Jac.PC02
+ */
+public class Sistema_cadastro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Chamando tela cadastro
+        Tela_login tela =  new Tela_login();
+        tela.setVisible(true);
+        tela.setLocationRelativeTo(null);
+        
+        
+        
+    }
+    
+}
